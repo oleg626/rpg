de0bbb303671c4838ab94ee1d00477cbafc33017
gotrari <i.generation@yandex.ru>
2023-07-21 21:21:48 +0300

    UI upd, skillsystem upd. character selection in progtress

--- Assets/Scripts/Scripts/MageSkillSystem.cs
+++ Assets/Scripts/Scripts/MageSkillSystem.cs
@@ -1,31 +1,40 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class SkillSystem : MonoBehaviour
+public class MageSkillSystem : MonoBehaviour
 {
     // Start is called before the first frame update
+
     public GameObject abillityObj;
-    public KeyCode skillKey;
+    public KeyCode skillKey1;
+    public KeyCode skillKey2;
     public GameObject greenCursor;
     public float coolDown = 5f;
     float nextSkillUse;
     public bool isActive = false;
     private Vector3 mousePos;
     private KeyCode cancelSkill = KeyCode.Escape;
+   
+
      void Start()
     {
-        greenCursor.GetComponent<Renderer>().enabled = false;   
+        greenCursor.GetComponent<Renderer>().enabled = false;
+      
+    
+        
     }
     void Update()
     {
+       
         mousePos = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0);
 
         if (Time.time > nextSkillUse)
         {
             
-            if (Input.GetKeyDown(skillKey))
+            if (Input.GetKeyDown(skillKey1))
             {
                 greenCursor.GetComponent <Renderer>().enabled = true;
                 Cursor.visible = false;
@@ -44,22 +53,27 @@ public class SkillSystem : MonoBehaviour
             nextSkillUse = Time.time + coolDown;
         }
 
-        
+        if(Input.GetKey(skillKey2))
+        {
+            
+        }
+   
+
+
 
 
     }
     void UseSkill()
     {
-        
-        
-            
-                
+
                 greenCursor.GetComponent<Renderer>().enabled = false;
                 Cursor.visible = true;
                 Instantiate(abillityObj, mousePos, Quaternion.identity);
                 isActive = false;
-            
-        
+
     }
+
+
+
    
 }
